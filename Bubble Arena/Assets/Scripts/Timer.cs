ce093bd1a39a85f849c08b347b033289cd517f57
Idris OUJDID <idris.oujdid@edu.esiee-it.fr>
2025-01-25 18:47:53 +0100

    first for timer of best game but it's true, need a condition for this end game but it's fonctionnaly

--- Bubble Arena/Assets/Scripts/Timer.cs
+++ Bubble Arena/Assets/Scripts/Timer.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Timer : MonoBehaviour
 {
+    private float timeRemaining = 3;
+    private bool timerIsRunning = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerIsRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining += Time.deltaTime;
+                if (timeRemaining <= 0)
+                {
+                    timeRemaining = 0;
+                    timerIsRunning = false;
+                }
+                Debug.Log("Time remaining: " + timeRemaining);
+            }
+            else
+            {
+                Debug.Log("Time has run out!");
+                timerIsRunning = false;
+            }
+        }
     }
 }
