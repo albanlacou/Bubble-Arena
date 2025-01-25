cedfb3c1390e586617acbefe7f3148d7f8c22e88
Alban <lacou.alban@gmail.com>
2025-01-25 03:12:59 +0100

    ajout de la boucle principale

--- Bubble Arena/Assets/Scripts/Player.cs
+++ Bubble Arena/Assets/Scripts/Player.cs
@@ -4,31 +4,84 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public int speed = 1;
 
-    private Rigidbody rb;
+    public Rigidbody rb;
 
     private Vector3 oldPosition;
 
+    public Vector3 force;
+
+    private float mouvementHorizontal;
+
+    private float mouvementVertical;
+
+    public bool isPlayerOne = false;
+
+    public Vector3 vectorDirecteur;
+
+    public Vector3 oldVelocity;
+
+    [SerializeField] private float vitesse = 150f; // Vitesse de déplacement
     // Start is called before the first frame update
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
-        rb.AddForce(new Vector3(10, 0, 0), ForceMode.Impulse);
+        //rb.AddForce(force, ForceMode.Impulse);
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 position = gameObject.transform.position;
+        vectorDirecteur = new Vector3(oldPosition.x - position.x, oldPosition.y - position.y, oldPosition.z - position.z);
+        if (isPlayerOne)
+        {
+            mouvementHorizontal = Input.GetAxis("Horizontal");
+            mouvementVertical = Input.GetAxis("Vertical");
+        }
+        else
+        {
+            mouvementHorizontal = Input.GetAxis("Player2Horizontal");
+            mouvementVertical = Input.GetAxis("Player2Vertical");
+        }
+        
+
         oldPosition = gameObject.transform.position;
+
+        oldVelocity = rb.velocity;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Vector3 position = gameObject.transform.position;
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            // Calculer la direction opposée
+            Vector3 collisionDirection = transform.position - collision.transform.position;
+            collisionDirection.Normalize(); // Normaliser pour obtenir une direction unitaire
+            Vector3 relativeVelocity = collision.relativeVelocity;
+            float test = Mathf.Abs(rb.velocity.x) + Mathf.Abs(rb.velocity.y);
+            Debug.Log(isPlayerOne +" : "+ rb.velocity.magnitude);
 
-        Vector3 vectorDirecteur = new Vector3(oldPosition.x - position.x,oldPosition.y - position.y, oldPosition.z - position.z);
+            
+            // Appliquer une force opposée
+            rb.AddForce(collisionDirection * 6, ForceMode.Impulse);
 
-        rb.AddForce(vectorDirecteur, ForceMode.Impulse);
+
+            
+        }
+        
+       
+
+    }
+
+    void FixedUpdate()
+    {
+       
+            rb.AddForce( new Vector3(mouvementHorizontal * speed, mouvementVertical*speed,0), ForceMode.Acceleration);
+           
+       
+        
         
     }
 }
