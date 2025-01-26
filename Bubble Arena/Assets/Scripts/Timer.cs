ed1d687f7f5308f43409a032176dade1601ed560
Idris OUJDID <idris.oujdid@edu.esiee-it.fr>
2025-01-26 02:49:52 +0100

    add a ui text & textMeshPro for timer remaining

--- Bubble Arena/Assets/Scripts/Timer.cs
+++ Bubble Arena/Assets/Scripts/Timer.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using TMPro; 
 
 public class Timer : MonoBehaviour
 {
-    private float timeRemaining = 0;
+    private float timeRemaining = 20;
     private bool timerIsRunning = false;
+    public TMP_Text textMeshPro;
 
     // Start is called before the first frame update
     void Start()
@@ -21,22 +23,25 @@ public class Timer : MonoBehaviour
         {
             if (timerIsRunning)
             {
-                if (timeRemaining <= 10.00000)
+                if (timeRemaining <= 20)
                 {
-                    timeRemaining += Time.deltaTime;
+                    timeRemaining-= Time.deltaTime;
                     if (timeRemaining <= 0)
                     {
                         timeRemaining = 0;
                         timerIsRunning = false;
                     }
-                    Debug.Log("Time remaining: " + timeRemaining);
+                    // Debug.Log("Time remaining: " + timeRemaining);
+                    textMeshPro.text = "Time remaining : " + (int)timeRemaining + "s";
                 }
                 else
                 {
-                    Debug.Log("Time has game out ! ");
+                    // Debug.Log("Time has game out ! ");
+                    textMeshPro.text = "This has game out" + (int)timeRemaining;
                     timerIsRunning = false;
                 }
             }
+
         }
     }
 }
