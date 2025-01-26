fd0a0ad46835e300e0b790b65035c439ab1df389
Idris OUJDID <idris.oujdid@edu.esiee-it.fr>
2025-01-26 11:14:20 +0100

    update Timer.cs

--- Bubble Arena/Assets/Scripts/Timer.cs
+++ Bubble Arena/Assets/Scripts/Timer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
-using TMPro; 
+using TMPro;
 
 public class Timer : MonoBehaviour
 {
@@ -25,20 +25,14 @@ public class Timer : MonoBehaviour
             {
                 if (timeRemaining <= 20)
                 {
-                    timeRemaining-= Time.deltaTime;
+                    timeRemaining -= Time.deltaTime;
                     if (timeRemaining <= 0)
                     {
-                        timeRemaining = 0;
                         timerIsRunning = false;
+                        textMeshPro.text = "This has game out" + (int)timeRemaining;
                     }
                     // Debug.Log("Time remaining: " + timeRemaining);
                     textMeshPro.text = "Time remaining : " + (int)timeRemaining + "s";
-                }
-                else
-                {
-                    // Debug.Log("Time has game out ! ");
-                    textMeshPro.text = "This has game out" + (int)timeRemaining;
-                    timerIsRunning = false;
                 }
             }
 
